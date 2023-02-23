b1d7b84770c0594ab815f498979e4033ca52761e
TROISIDesign <michael@troisi.com>
2023-02-23 08:47:19 -0500

    Add Contact Page to App

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default () => (
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
       <div className="content">
         <Routes />
---
+++ src/containers/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+    <div>
+        <h1>
+            Contact Us 
+        </h1>
+        <span>
+            <form>
+                <label>
+                    <input type="text" name="email@address.com" />
+                </label>
+                <input type="submit" name="Send" />
+            </form>
+        </span>
+    </div>
+)
