ac57cba751999d955ae7ea7f55361fdf7766c335
TROISIDesign <michael@troisi.com>
2023-02-23 09:05:47 -0500

    Revert style for Contact Page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,16 +1,18 @@
 import React from "react";
-import './style.css'
+
 
 export default () => (
     <div>
         <h1>
             Contact Us 
         </h1>
+        <div>
         <form>
             <label>
-                <input type="text" name="email@address.com" />
+                <input type="text" name="email" />
             </label>
             <input type="submit" name="Send" />
         </form>
+        </div>
     </div>
 )
