a7dd2585687904cbee82808e4d2ba18bafdd80c3
TROISIDesign <michael@troisi.com>
2023-02-23 08:50:09 -0500

    Add route path for contact page

--- static.config.js
+++ static.config.js
@@ -29,6 +29,10 @@ export default {
         }))
       },
       {
+        path: "/contact",
+        component: "src/containers/Contact"
+      },
+      {
         is404: true,
         component: "src/containers/404"
       }
