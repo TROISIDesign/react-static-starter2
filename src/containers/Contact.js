f3f15364c67fb034748c5a797a8b180383de839e
TROISIDesign <michael@troisi.com>
2023-02-23 08:58:38 -0500

    Test new stylesheet on Contact page

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,17 +1,16 @@
 import React from "react";
+import './style.css'
 
 export default () => (
     <div>
         <h1>
             Contact Us 
         </h1>
-        <span>
-            <form>
-                <label>
-                    <input type="text" name="email@address.com" />
-                </label>
-                <input type="submit" name="Send" />
-            </form>
-        </span>
+        <form>
+            <label>
+                <input type="text" name="email@address.com" />
+            </label>
+            <input type="submit" name="Send" />
+        </form>
     </div>
 )
